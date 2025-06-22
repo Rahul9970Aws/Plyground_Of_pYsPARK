1dc1a614278c26c6a688bce7f0d45926ae997342
Nayanananda S M <46737705+Nayan2695@users.noreply.github.com>
2025-06-22 15:51:28 +0530

    Update customer_DDL_TYPE1.sql

--- SQL_ENV/Nayan_SCDTYPES/src/customer_DDL_TYPE1.sql
+++ SQL_ENV/Nayan_SCDTYPES/src/customer_DDL_TYPE1.sql
@@ -29,3 +29,17 @@ CREATE TABLE webshop.dim_customer (
 	ins_dt varchar NULL,
 	upd_dt varchar NULL
 );
+
+--type dim table
+CREATE TABLE webshop.dim_customer_type2 (
+    id INT NOT NULL,                
+    firstname TEXT,
+    lastname TEXT,
+    gender TEXT,
+    email TEXT,
+    dateofbirth DATE,
+    currentaddressid INT,
+    is_current BOOLEAN DEFAULT TRUE,       
+    start_date DATE DEFAULT CURRENT_DATE,   
+    end_date DATE DEFAULT NULL             
+);
